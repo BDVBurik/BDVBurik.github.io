f13b2879a6ca09d5488879f6c5ff995bc46d2c29
BDVBurik <bdvburik@gmail.com>
2024-06-20 13:11:47 +0300

    Update franshrezka.js
    :8443

--- franshrezka.js
+++ franshrezka.js
@@ -3,7 +3,7 @@
   ////rezkacomment без спойлеров
   ("use strict");
   const tmdbApiUrl = "https://api.themoviedb.org/3/";
-  const kp_prox = "https://worker-patient-dream-26d7.bdvburik.workers.dev/";
+  const kp_prox = "https://worker-patient-dream-26d7.bdvburik.workers.dev:8443/";
   const urlEndTMDB = "?language=en-US&api_key=4ef0d7355d9ffb5151e987764708ce96";
   var namemovie;
   var www;
