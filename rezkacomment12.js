5554a8cb68efaa76550b009c2ff4e6e48d99aaf0
BDVBurik <bdvburik@gmail.com>
2025-12-12 20:16:01 +0200

    Update rezkacomment12.js

--- rezkacomment12.js
+++ rezkacomment12.js
@@ -200,25 +200,21 @@
     `);
 
     const enabled = Lampa.Controller.enabled().name;
-
     Lampa.Modal.open({
-      title: "",
+      title: ``,
       html: modal,
       size: "large",
       style: "margin-top:10px;",
       mask: !0,
       onBack: function () {
-        Lampa.Modal.close();
-        Lampa.Controller.toggle(enabled);
+        Lampa.Modal.close(), Lampa.Controller.toggle(enabled);
         $(".modal--large").remove();
         www = "";
       },
       onSelect: function () {},
     });
-Lampa.Loading.stop();
-
     $(".modal__head").after(
-      `${namemovie}`
+      `${namemovie}<button class="selector "  tabindex="0" style = "float: right;" type="button"  onclick="$('.modal--large').remove()"  data-dismiss="modal">&times;</button>`
     );
   }
 
@@ -248,7 +244,5 @@ Lampa.Loading.stop();
     });
   }
 
-  if (!window.comment_plugin) startPlugin();
-})();
   if (!window.comment_plugin) startPlugin();
 })();
