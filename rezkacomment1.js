349c4b068723314aac9b97ea553905c77979bfb1
BDVBurik <bdvburik@gmail.com>
2025-12-15 01:50:27 +0200

    urlEndTMDB

--- rezkacomment1.js
+++ rezkacomment1.js
@@ -5,7 +5,7 @@
 
   let year;
   let namemovie;
-  const urlEndTMDB = "?language=ru-RU&api_key=4ef0d7355d9ffb5151e987764708ce96";
+  const urlEndTMDB = "?language=en-US&api_key=4ef0d7355d9ffb5151e987764708ce96";
 
   const tmdbApiUrl = "https://api.themoviedb.org/3/";
   let kp_prox = "https://worker-patient-dream-26d7.bdvburik.workers.dev:8443/";
