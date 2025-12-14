87111e927d15b1758de4f83c9894e6099ec86d6b
BDVBurik <bdvburik@gmail.com>
2025-12-14 22:09:10 +0200

    qwe

--- title2.js
+++ title2.js
@@ -16,15 +16,14 @@
       alt.find((t) => t.type === "Transliteration")?.title || orig;
 
     // Альтернативные переводы
-    let ruAlt = alt.find(
-      (t) => t.iso_3166_1 === "RU" || t.iso_639_1 === "ru"
-    )?.title;
-    let ukAlt = alt.find(
-      (t) => t.iso_3166_1 === "UA" || t.iso_639_1 === "uk"
-    )?.title;
+    let ruAlt = alt.find((t) => t.iso_639_1?.toLowerCase() === "ru")?.title;
+    let ukAlt = alt.find((t) => t.iso_639_1?.toLowerCase() === "uk")?.title;
     let enAlt =
-      alt.find((t) => t.iso_3166_1 === "US" || t.iso_639_1 === "en")?.title ||
-      alt.find((t) => t.iso_3166_1 === "EN")?.title;
+      alt.find(
+        (t) =>
+          t.iso_3166_1?.toLowerCase() === "us" ||
+          t.iso_639_1?.toLowerCase() === "en"
+      )?.title || alt.find((t) => t.iso_3166_1?.toLowerCase() === "en")?.title;
 
     // Проверяем кэш TMDB
     const now = Date.now();
@@ -51,16 +50,18 @@
 
         if (!ruAlt)
           ruAlt = translations.find(
-            (t) => t.iso_3166_1 === "RU" || t.iso_639_1 === "ru"
+            (t) => (t) => t.iso_3166_1?.toLowerCase() === "ru"
           )?.data.title;
         if (!ukAlt)
           ukAlt = translations.find(
-            (t) => t.iso_3166_1 === "UA" || t.iso_639_1 === "uk"
+            (t) => (t) => t.iso_3166_1?.toLowerCase() === "ua"
           )?.data.title;
         if (!enAlt)
           enAlt =
             translations.find(
-              (t) => t.iso_3166_1 === "US" || t.iso_639_1 === "en"
+              (t) => (t) =>
+                t.iso_3166_1?.toLowerCase() === "us" ||
+                t.iso_639_1?.toLowerCase() == "en"
             )?.data.title ||
             translations.find((t) => t.iso_3166_1 === "EN")?.data.title;
 
