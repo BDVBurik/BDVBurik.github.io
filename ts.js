222f44f5138a90908ce208c7da1f7c2f97fdaa69
BDVBurik <bdvburik@gmail.com>
2026-01-04 22:37:17 +0200

    Fix fetch URL by removing 'http://' prefix

--- ts.js
+++ ts.js
@@ -28,7 +28,7 @@
   // -------- проверка сервера --------
   async function pingServer(url, index) {
     try {
-      await fetch(`http://${url}/echo`);
+      await fetch(`${url}/echo`);
       Lampa.Storage.set(`FreeServ_${index+1}`, url);
     } catch (e) {
       Lampa.Storage.set(`FreeServ_${index+1}`, 'NotFound');
