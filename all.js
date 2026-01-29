e41fb4ea7cd407067560d21bc94bb2d42bf2a1f3
BDVBurik <bdvburik@gmail.com>
2026-01-29 18:57:32 +0200

    all

---
+++ all.js
@@ -0,0 +1,22 @@
+(function () {
+  "use strict";
+
+  var timer = setInterval(function () {
+    if (typeof Lampa !== "undefined") {
+      clearInterval(timer);
+
+      Lampa.Utils.putScriptAsync(
+        [
+          "https://bdvburik.github.io/ts.js",
+          "https://bdvburik.github.io/labelse.js",
+          "https://bdvburik.github.io/sub.js",
+          "https://bdvburik.github.io/kp_rating.js",
+          "https://bdvburik.github.io/title.js",
+          "https://bdvburik.github.io/rezkacomment.js",
+          "https://bdvburik.github.io/ads.js",
+        ],
+        function () {},
+      );
+    }
+  }, 200);
+})();
