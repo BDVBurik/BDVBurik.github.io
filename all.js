96bd6d7f0be4d1d948db3b9b73af185a82716253
BDVBurik <bdvburik@gmail.com>
2026-05-10 16:29:06 +0300

    Update all.js

--- all.js
+++ all.js
@@ -28,7 +28,7 @@
           // "https://ipavlin98.github.io/lmp-plugins/anime-skip.js",
           // "https://ipavlin98.github.io/lmp-plugins/season-fix.js",
 
-          //"https://igorek1986.github.io/lampa-plugins/myshows.js",
+          "https://lampame.github.io/main/trakttv.js",
 
          // "http://bwa.to/oeogfeb",
 
